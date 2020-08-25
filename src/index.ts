d79ec8189f579531428d017a281622509ce40ba2
kot-shrodingera <kot.shrodingera@gmail.com>
2020-08-25 20:47:47 +0600

    Обновление до v1.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import '@kot-shrodingera-team/worker-declaration/workerCheck';
 import { log } from '@kot-shrodingera-team/germes-utils';
-import showStake from './show_stake';
 import getStakeInfo from './worker_callbacks/getStakeInfo';
 import setStakeSum from './worker_callbacks/setStakeSum';
 import doStake from './worker_callbacks/doStake';
 import checkCouponLoading from './worker_callbacks/checkCouponLoading';
 import checkStakeStatus from './worker_callbacks/checkStakeStatus';
-import initialize from './initialization';
 import afterSuccesfulStake from './worker_callbacks/afterSuccesfulStake';
 import fastLoad from './fastLoad';
+import initialize from './initialization';
+import showStake from './show_stake';
 
 worker.SetCallBacks(
   log,
