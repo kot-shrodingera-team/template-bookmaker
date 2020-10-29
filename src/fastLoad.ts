697a51bffc0862e041c040c0b2448a558250feec
kot-shrodingera <kot.shrodingera@gmail.com>
2020-10-30 04:26:48 +0600

    fix(fastLoad): выполнение showStake по умолчанию

--- src/fastLoad.ts
+++ src/fastLoad.ts
@@ -1,9 +1,10 @@
 import { log } from '@kot-shrodingera-team/germes-utils';
 import { version } from '../package.json';
+import showStake from './show_stake';
 
 const fastLoad = async (): Promise<void> => {
   log(`Быстрая загрузка (${version})`, 'steelblue');
-  worker.Helper.LoadUrl(worker.EventUrl);
+  showStake();
 };
 
 export default fastLoad;
