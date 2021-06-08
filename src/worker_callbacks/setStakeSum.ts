5c90f62147d8255c757f75af5d723a5323f61b7c
kot-shrodingera <kot.shrodingera@gmail.com>
2021-06-08 21:56:22 +0600

    feat(setStakeSum): обновление генератора, добавление clearStakeSum

--- src/worker_callbacks/setStakeSum.ts
+++ src/worker_callbacks/setStakeSum.ts
@@ -1,4 +1,6 @@
-import setStakeSumGenerator from '@kot-shrodingera-team/germes-generators/worker_callbacks/setStakeSum';
+import setStakeSumGenerator, {
+  clearStakeSumGenerator,
+} from '@kot-shrodingera-team/germes-generators/worker_callbacks/setStakeSum';
 import { sumInputSelector } from '../stake_info/getCurrentSum';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -6,15 +8,19 @@ const preInputCheck = (sum: number): boolean => {
   return true;
 };
 
-const setStakeSum = setStakeSumGenerator({
+const setStakeSumOptions = {
   sumInputSelector,
   alreadySetCheck: {
     falseOnSumChange: false,
   },
   preInputCheck,
-  inputType: 'fireEvent',
+  inputType: 'fireEvent' as 'fireEvent' | 'react' | 'nativeInput',
   fireEventNames: ['input'],
   context: () => document,
-});
+};
+
+const setStakeSum = setStakeSumGenerator(setStakeSumOptions);
+
+export const clearStakeSum = clearStakeSumGenerator(setStakeSumOptions);
 
 export default setStakeSum;
