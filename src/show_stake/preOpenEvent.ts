96152e396f30f29de250957338db3bec6689f733
kot-shrodingera <kot.shrodingera@gmail.com>
2021-07-27 13:35:27 +0300

    feat(preOpenEvent): лог адресов при ошибке открытого хоста

--- src/show_stake/preOpenEvent.ts
+++ src/show_stake/preOpenEvent.ts
@@ -9,6 +9,7 @@ import { balanceReady, updateBalance } from '../stake_info/getBalance';
 const preOpenEvent = async (): Promise<void> => {
   if (!checkBookerHost()) {
     log('Открыта не страница конторы (или зеркала)', 'crimson');
+    log(`${window.location.href} !== ${worker.BookmakerMainUrl}`, 'crimson');
     window.location.href = new URL(worker.BookmakerMainUrl).href;
     throw new NewUrlError('Открываем страницу БК');
   }
